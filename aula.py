e4ab2a6d0a63ca85cb8439f5d36a863ad9200bb5
lua55 <luanabfsilva774@gmail.com>
2024-08-09 14:50:59 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,20 @@
+# ATIVIDADE1
+
+# for numero in range(2,21,2)
+# print(numero)
+
+# ATIVIDADE2
+
+# for numero in range(5,51,5)
+# print(numero)
+
+# ATIVIDADE3
+
+# MULT = 1.5*6.8
+# print(MULT)
+
+# ATIVIDADE4
+
+nome = print('digite o seu nome')
+comprimento = ('seja bem vindo')
+
